90b45ca5f66fc4ebc36e1377c0209d2ba5e81bd3
Denis Roenko <denis@roenko.ru>
2020-05-16 19:44:58 +0300

    add random colors

--- RandomView/RectangleView.swift
+++ RandomView/RectangleView.swift
@@ -9,8 +9,18 @@
 import SwiftUI
 
 struct RectangleView: View {
-    var color = Color(.black)
-    
+    var color: Color {
+        switch Int.random(in: 0...6) {
+            case 0: return Color(.red)
+            case 1: return Color(.orange)
+            case 2: return Color(.yellow)
+            case 3: return Color(.green)
+            case 4: return Color(.cyan)
+            case 5: return Color(.blue)
+            case 6: return Color(.purple)
+            default: return Color(.black)
+        }
+    }
     var body: some View {
         Rectangle()
             .foregroundColor(self.color)
