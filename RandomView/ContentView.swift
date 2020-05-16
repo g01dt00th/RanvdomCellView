732243b179d244817c0ff595952e1898bae847c6
Denis Roenko <denis@roenko.ru>
2020-05-16 18:45:06 +0300

    Initial Commit

---
+++ RandomView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RandomView
+//
+//  Created by g01dt00th on 16.05.2020.
+//  Copyright © 2020 Denis Roenko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
